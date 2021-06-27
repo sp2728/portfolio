f74ee4fc09f27b8cdc737ea76bafafbed52b6784
sp2728 <sp2728@njit.edu>
2021-06-26 21:07:54 -0400

    minor update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this._snackBar.open('Website is under development','OK', {
       verticalPosition: 'top',
-    })
+    });
   }
 
   getCurrentSection(){
